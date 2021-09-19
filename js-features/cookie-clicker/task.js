c6ec211299f949d6be2bd2b7e0e3df4b5b607893
lan-mak <lan-mak@ya.ru>
2021-09-20 00:26:07 +0300

    ver 0.03a

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -4,10 +4,19 @@ let cockie = document.getElementById('cookie');
 let counter = document.getElementById('clicker__counter');
 let sizeImage = 1;
 let scoreClick = document.getElementById('clicker__score');
+let arrScore = [];
 
-let click_1, click_2;
+function score() {
+  arrScore.push(Date.now());
 
-function checkScore() {
+  if (arrScore.length > 1) {
+    let speedClick = 1/((arrScore[1] - arrScore[0])/1000)
+    scoreClick.textContent = speedClick.toFixed(2)
+    arrScore.shift()
+  }
+}
+
+/* function checkScore() {
   if (+counter.textContent === 0) {
     scoreClick.textContent = 0;
     click_1 = Date.now();
@@ -16,17 +25,19 @@ function checkScore() {
     let score = +counter.textContent/new Date(click_2 - click_1).getSeconds();
     scoreClick.textContent = score.toFixed(2);
   }
-}
+} */
 
 cockie.onclick = () => {
   if (sizeImage === 1) {
     cockie.width = 180;
     sizeImage = 0;
-    checkScore();
+    score()
+    // checkScore();
   } else {
     cockie.width = 200;
     sizeImage = 1;
-    checkScore();
+    score()
+    // checkScore();
   }
   counter.textContent = +counter.textContent + 1;
 }
