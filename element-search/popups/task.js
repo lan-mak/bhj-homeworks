4623c65c09476c25a7501cfaa56349df06d42787
lan-mak <lan-mak@ya.ru>
2021-09-26 14:54:47 +0300

    popups ver 0.01

---
+++ element-search/popups/task.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let modalMain = document.querySelector('#modal_main');
+let modalSuccess = document.querySelector('#modal_success')
+
+let showSuccess = document.querySelector('.show-success')
+let closeElem = document.querySelectorAll('.modal__close')
+closeElem.forEach(close)
+
+modalMain.className = 'modal modal_active';
+
+function close(item) {
+  showSuccess.onclick = function() {
+    modalSuccess.className = 'modal modal_active'
+  }
+
+  item.onclick = function() {
+    modalMain.className = 'modal'
+    modalSuccess.className = 'modal'
+  }
+}
