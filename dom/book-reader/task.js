dc75307d1161df675f8a537bc01c98347ddace53
lan-mak <lan-mak@ya.ru>
2021-10-19 22:54:42 +0300

    dom v0.04a

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -10,19 +10,26 @@ function onClick(event) {
     event.preventDefault();
     let controlElement = event.target;
     let book = this.closest('#book');
-    let classFontSize = `book_fs-${this.dataset.size}`
+    let classFontSize = `book_fs-${this.dataset.size}`;
 
     if (controlElement.classList.contains('font-size_small') && !book.classList.contains('classFontSize')) {
-        book.className = 'book';
+        changeControl();
         book.classList.add(classFontSize);
     }
 
-    if (controlElement.classList.contains('font-size_active')) {
-        book.className = 'book';
+    if (controlElement.classList.length === 1) {
+        changeControl();
     }
 
     if (controlElement.classList.contains('font-size_big') && !book.classList.contains('classFontSize')) {
-        book.className = 'book';
+        changeControl();
         book.classList.add(classFontSize);
+    }
+
+    function changeControl() {
+        let controlActive = document.querySelector('.font-size_active');
+        controlActive.classList.remove('font-size_active');
+        book.className = 'book';
+        controlElement.classList.add('font-size_active');
     }
 }
